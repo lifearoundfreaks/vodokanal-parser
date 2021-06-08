5881e418ccf79fee9f6c185600363671ea890c31
Andrew <life.around.freaks@gmail.com>
2021-06-08 14:25:03 +0300

    Initial commit

---
+++ main.py
@@ -0,0 +1,164 @@
+from os import listdir, path
+from pathlib import Path
+import pandas as pd
+
+from reader import Reader, ContractSorter
+from const import (
+    HIDDEN_COLUMNS,
+    SHARED_DEPARTMENTS,
+    REGIONAL_DEPARTMENTS,
+    MISSING_FOLDER,
+    RESULT_FILE_NAME,
+    MONTH_PROMPT,
+    REGISTRY_HEADER,
+    MONTH_HEADER,
+    TABLE_HEADERS,
+    TABLE_TOTALS,
+    TABLE_FOOTER,
+    DATA_START_ROW,
+    FINISH_PROMPT,
+    WAIT_MESSAGE,
+    ERROR_MESSAGE,
+    SHEET_NAME,
+    LAST_TABLE_COLUMN,
+    TABLE_START_ROW,
+    JOB_NAME_COLUMN,
+    SECTION_TEMPLATE,
+    HEADER_TO_MERGE,
+    COLUMN_WIDTHS,
+    HIDDEN_COLUMNS,
+    FOOTER_TO_MERGE,
+)
+
+
+def find_unique_filename(month):
+
+    counter = 0
+    last_attempted_name = RESULT_FILE_NAME.format(
+        month=month, number='', format='xlsx'
+    )
+    while path.exists(last_attempted_name):
+        counter += 1
+        last_attempted_name = RESULT_FILE_NAME.format(
+            month=month, number=counter, format='xlsx'
+        )
+    return last_attempted_name
+
+
+cwd = Path.cwd()
+
+month = input(MONTH_PROMPT)
+dept_names = [*SHARED_DEPARTMENTS.values(), *REGIONAL_DEPARTMENTS.values()]
+
+files_to_process = []
+add_file = files_to_process.append
+
+for folder in dept_names:
+
+    try:
+        for filename in listdir(folder):
+            if filename.endswith('.xls') or filename.endswith('.xlsx'):
+                add_file(cwd / folder / filename)
+    except FileNotFoundError:
+        print(MISSING_FOLDER.format(folder=folder))
+
+print(WAIT_MESSAGE)
+
+contracts = []
+add_contracts = contracts.extend
+
+for filepath in files_to_process:
+
+    try:
+        reader = Reader(pd.read_excel(filepath, header=None).iterrows(), month)
+        add_contracts(reader.get_contracts())
+    except Exception:
+        print(ERROR_MESSAGE.format(filepath=filepath, row=reader.current_row))
+
+data = [[registry_part] for registry_part in REGISTRY_HEADER]
+data.append([MONTH_HEADER.format(month=month)])
+data.extend([[], []])
+data.append(TABLE_HEADERS)
+data.append(range(1, len(TABLE_HEADERS) + 1))
+data.extend(ContractSorter(contracts).get_dataframe_data())
+data.append([])
+data.append([
+    cell.format(start=DATA_START_ROW, table_size=len(data)
+) for cell in TABLE_TOTALS])
+data.extend([[], []])
+data.extend(TABLE_FOOTER)
+last_row = len(data) - len(TABLE_FOOTER) - 2
+
+dataframe = pd.DataFrame(data)
+
+filename = find_unique_filename(month)
+
+writer = pd.ExcelWriter(filename, engine='xlsxwriter')
+
+# region TABLE FORMATTING
+workbook  = writer.book
+default_format = workbook.formats[0]
+default_format.set_font_size(10)
+default_format.set_align('center')
+default_format.set_align('vcenter')
+default_format.set_text_wrap()
+worksheet = workbook.add_worksheet(SHEET_NAME)
+writer.sheets[SHEET_NAME] = worksheet
+
+
+def simple_format(**params):
+
+    return {
+        'type': 'no_errors',
+        'format': workbook.add_format(params),
+    }
+
+
+def text_format(criteria, value, **params):
+
+    return {
+        'type': 'text',
+        'criteria': criteria,
+        'value': value,
+        'format': workbook.add_format(params),
+    }
+
+table_range = f'A{TABLE_START_ROW}:{LAST_TABLE_COLUMN}{last_row}'
+worksheet.conditional_format(table_range, simple_format(border=True))
+
+header_range = f'A1:{LAST_TABLE_COLUMN}{TABLE_START_ROW+1}'
+worksheet.conditional_format(header_range, simple_format(bold=True))
+
+dept_range = f'{JOB_NAME_COLUMN}{TABLE_START_ROW}:{JOB_NAME_COLUMN}{last_row}'
+
+for keyword in [*dept_names, SECTION_TEMPLATE[:2]]:
+    worksheet.conditional_format(dept_range, text_format(
+        'begins with', keyword, bold=True
+    ))
+
+for _range in HEADER_TO_MERGE:
+
+    worksheet.merge_range(_range, "")
+
+for row, _range in enumerate(FOOTER_TO_MERGE, last_row+3):
+
+    worksheet.merge_range(_range.format(row=row), "")
+
+for i, width in enumerate(COLUMN_WIDTHS):
+
+    if i in HIDDEN_COLUMNS:
+        worksheet.set_column_pixels(i, i, width+1, None, {'hidden': True})
+    else:
+        worksheet.set_column_pixels(i, i, width+1)
+
+# endregion TABLE FORMATTING
+
+dataframe.to_excel(
+    writer,
+    header=None,
+    index=None,
+    sheet_name=SHEET_NAME
+)
+writer.save()
+
+input(FINISH_PROMPT)
---
+++ reader.py
@@ -0,0 +1,178 @@
+from dataclasses import dataclass
+from collections import defaultdict
+
+
+from const import (
+    SHARED_DEPARTMENTS,
+    REGIONAL_DEPARTMENTS,
+    JOB_SECTIONS,
+    SECTION_NUMBERS,
+    CONTRACT_START_TRIGGER,
+    DATA_START_ROW,
+    SECTION_TEMPLATE,
+    TYPE_TEMPLATE,
+    COST_COLUMN,
+    CONTRACT_NUMBER_SIGNIFIER,
+)
+
+
+@dataclass
+class Contract():
+    job_type: str
+    job_section: str
+    department: str
+    number: str
+    address: str
+    job_name: str
+
+
+class Reader:
+
+    MAIN_CONTENT_COLUMN = 0
+    EMPTY_COL_SIGNIFIER = 'nan'
+    ADDRESS_COLUMN = 2
+    INITIAL_ROW = 0
+
+    def __init__(self, row_iter, month):
+
+        self.iter = row_iter
+        self.month = month
+        self.current_row = self.INITIAL_ROW
+
+    def get_next(self):
+
+        self.current_row, row = next(self.iter)
+
+        return row
+
+    def get_main_content(self, row):
+
+        return str(row[self.MAIN_CONTENT_COLUMN]).strip()
+
+    def get_address(self, row):
+
+        return str(row[self.ADDRESS_COLUMN]).strip()
+
+    def parse_contract_data(self):
+
+        self.get_next()
+
+        department_info = self.get_main_content(self.get_next())
+        split_department = department_info.split()
+        job_type = " ".join(split_department[:-2])
+        job_section = JOB_SECTIONS[job_type]
+        job_type = job_type.capitalize()
+        department = " ".join(split_department[-2:])
+
+        shared_department = self.get_main_content(self.get_next())
+        department = SHARED_DEPARTMENTS[shared_department] \
+            if shared_department != self.EMPTY_COL_SIGNIFIER \
+            else REGIONAL_DEPARTMENTS[department]
+
+        number = self.get_main_content(
+            self.get_next()
+        ).split(CONTRACT_NUMBER_SIGNIFIER)[-1]
+
+        address = self.get_address(self.get_next())
+
+        job_name = []
+        while True:
+
+            string = self.get_main_content(self.get_next())
+
+            if self.month in string:
+
+                return Contract(
+                    job_type, job_section, department, number, address,
+                    ", ".join(job_name)
+                )
+
+            job_name.append(string)
+
+    def get_contracts(self):
+
+        contracts = []
+        add_contract = contracts.append
+
+        try:
+            while True:
+
+                if self.get_main_content(
+                    self.get_next()
+                ) == CONTRACT_START_TRIGGER:
+                    add_contract(self.parse_contract_data())
+
+        except StopIteration:
+
+            return contracts
+
+
+class ContractSorter:
+
+    def __init__(self, contracts):
+
+        self.sections = {
+            section: defaultdict(list) for section in JOB_SECTIONS.values()
+        }
+        self.types = {value: key for key, value in JOB_SECTIONS.items()}
+
+        for contract in contracts:
+            self.sections[contract.job_section][contract.department].append(
+                contract
+            )
+    
+    def get_dataframe_data(self):
+
+        result = []
+        add = result.append
+
+        current_row = DATA_START_ROW
+
+        for section, departments in sorted(
+            self.sections.items(), key=lambda item: SECTION_NUMBERS[item[0]]
+        ):
+            section_number = SECTION_NUMBERS[section]
+
+            add(['', '', SECTION_TEMPLATE.format(
+                name=section, number=section_number
+            )])
+
+            add(['', '', TYPE_TEMPLATE.format(
+                name=JOB_SECTIONS[self.types[section]], number=section_number
+            )])
+
+            current_row += 2
+
+            for department, contracts in departments.items():
+
+                start = current_row
+                end = current_row = current_row + len(contracts)
+
+                sum_str = f'&SUM({COST_COLUMN}{start}:{COST_COLUMN}{end})'
+
+                add(['', '', f'="{department}: "{sum_str}'])
+
+                for contract in contracts:
+
+                    add([
+                        '',
+                        contract.number,
+                        f'{contract.address}, {contract.job_name}',
+                        '',
+                        '',
+                        '',
+                        '',
+                        '',
+                        '',
+                        '',
+                        '',
+                        contract.department,
+                    ])
+
+                add([])
+                current_row += 2
+
+            add([])
+            current_row += 1
+
+        return result
